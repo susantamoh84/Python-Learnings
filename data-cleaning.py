43f879b47309f7ad57a73a7a32aee787cd7f1f22
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-09-22 02:16:26 +0530

    Create data-cleaning.py

---
+++ data-cleaning.py
@@ -0,0 +1,457 @@
+# Import pandas
+import pandas as pd
+
+# Read the file into a DataFrame: df
+df = pd.read_csv('dob_job_application_filings_subset.csv')
+
+# Print the head of df
+print(df.head())
+
+# Print the tail of df
+print(df.tail())
+
+# Print the shape of df
+print(df.shape)
+
+# Print the columns of df
+print(df.columns)
+
+# Print the head and tail of df_subset
+print(df_subset.head())
+print(df_subset.tail())
+
+# Print the info of df
+print(df.info())
+
+# Print the info of df_subset
+print(df_subset.info())
+
+# Print the value counts for 'Borough'
+print(df['Borough'].value_counts(dropna=False))
+
+# Print the value_counts for 'State'
+print(df.State.value_counts(dropna=False))
+
+# Print the value counts for 'Site Fill'
+print(df['Site Fill'].value_counts(dropna=False))
+
+# Import necessary modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create the boxplot
+df.boxplot(column='initial_cost', by='Borough', rot=90)
+
+# Display the plot
+plt.show()
+
+# Create and display the first scatter plot
+df.plot(kind='scatter', x='initial_cost', y='total_est_fee', rot=70)
+plt.show()
+
+# Create and display the second scatter plot
+df_subset.plot(kind='scatter', x='initial_cost', y='total_est_fee', rot=70)
+plt.show()
+
+# Print the head of airquality
+print(airquality.head())
+
+# Melt airquality: airquality_melt
+airquality_melt = pd.melt(airquality, id_vars=["Month", "Day"])
+
+# Print the head of airquality_melt
+print(airquality_melt.head())
+
+# Melt airquality: airquality_melt
+airquality_melt = pd.melt(airquality, id_vars=['Month','Day'], var_name='measurement', value_name='reading')
+
+# Print the head of airquality_melt
+print(airquality_melt.head())
+
+# Print the head of airquality_melt
+print(airquality_melt.head())
+
+# Pivot airquality_melt: airquality_pivot
+airquality_pivot = pd.pivot_table(airquality_melt, index=['Month','Day'], columns='measurement', values='reading')
+
+# Print the head of airquality_pivot
+print(airquality_pivot.head())
+
+# Print the index of airquality_pivot
+print(airquality_pivot.index)
+
+# Reset the index of airquality_pivot: airquality_pivot_reset
+airquality_pivot_reset = airquality_pivot.reset_index()
+
+# Print the new index of airquality_pivot_reset
+print(airquality_pivot_reset.index)
+
+# Print the head of airquality_pivot_reset
+print(airquality_pivot_reset.head())
+
+# Pivot airquality_dup: airquality_pivot
+airquality_pivot = pd.pivot_table(airquality_dup,index=['Month', 'Day'], columns='measurement', values='reading', aggfunc=np.mean)
+
+# Reset the index of airquality_pivot
+airquality_pivot = airquality_pivot.reset_index()
+
+# Print the head of airquality_pivot
+print(airquality_pivot.head())
+
+# Print the head of airquality
+print(airquality.head())
+
+# Melt tb: tb_melt
+tb_melt = pd.melt(tb, id_vars=['country', 'year'])
+
+# Create the 'gender' column
+tb_melt['gender'] = tb_melt.variable.str[0]
+
+# Create the 'age_group' column
+tb_melt['age_group'] = tb_melt.variable.str[1:]
+
+# Print the head of tb_melt
+print(tb_melt.head())
+
+# Melt ebola: ebola_melt
+ebola_melt = pd.melt(ebola, id_vars=['Date', 'Day'], var_name='type_country', value_name='counts')
+
+# Create the 'str_split' column
+ebola_melt['str_split'] = ebola_melt.type_country.str.split('_')
+
+# Create the 'type' column
+ebola_melt['type'] = ebola_melt['str_split'].str.get(0)
+
+# Create the 'country' column
+ebola_melt['country'] = ebola_melt['str_split'].str.get(1)
+
+# Print the head of ebola_melt
+print(ebola_melt.head())
+
+# Concatenate uber1, uber2, and uber3: row_concat
+row_concat = pd.concat([uber1, uber2, uber3])
+
+# Print the shape of row_concat
+print(row_concat.shape)
+
+# Print the head of row_concat
+print(row_concat.head())
+
+# Concatenate ebola_melt and status_country column-wise: ebola_tidy
+ebola_tidy = pd.concat([ebola_melt, status_country], axis=1)
+
+# Print the shape of ebola_tidy
+print(ebola_tidy.shape)
+
+# Print the head of ebola_tidy
+print(ebola_tidy.head())
+
+# Import necessary modules
+import glob
+import pandas as pd
+
+# Write the pattern: pattern
+pattern = '*.csv'
+
+# Save all file matches: csv_files
+csv_files = glob.glob(pattern)
+
+# Print the file names
+print(csv_files)
+
+# Load the second file into a DataFrame: csv2
+csv2 = pd.read_csv(csv_files[1])
+
+# Print the head of csv2
+print(csv2.head())
+
+# Create an empty list: frames
+frames = []
+
+#  Iterate over csv_files
+for csv in csv_files:
+
+    #  Read csv into a DataFrame: df
+    df = pd.read_csv(csv)
+    
+    # Append df to frames
+    frames.append(df)
+
+# Concatenate frames into a single DataFrame: uber
+uber = pd.concat(frames)
+
+# Print the shape of uber
+print(uber.shape)
+
+# Print the head of uber
+print(uber.head())
+
+# Merge the DataFrames: o2o
+o2o = pd.merge(left=site, right=visited, left_on='name', right_on='site')
+
+# Print o2o
+print(o2o)
+
+# Merge the DataFrames: m2o
+m2o = pd.merge(site, visited, left_on='name', right_on='site')
+
+# Print m2o
+print(m2o)
+
+# Merge site and visited: m2m
+m2m = pd.merge(left=site, right=visited, left_on='name', right_on='site')
+
+# Merge m2m and survey: m2m
+m2m = pd.merge(left=m2m, right=survey, left_on='ident', right_on='taken')
+
+# Print the first 20 lines of m2m
+print(m2m.head(20))
+
+# Convert the sex column to type 'category'
+tips.sex = tips.sex.astype('category')
+
+# Convert the smoker column to type 'category'
+tips.smoker = tips.smoker.astype('category')
+
+# Print the info of tips
+print(tips.info())
+#Excellent! By converting sex and smoker to categorical variables, the memory usage of the DataFrame went down from 13.4 KB to 10.1KB. 
+#This may seem like a small difference here, but when you're dealing with large datasets, the reduction in memory usage can be very significant!
+
+# Convert 'total_bill' to a numeric dtype
+tips['total_bill'] = pd.to_numeric(tips['total_bill'], errors='coerce')
+
+# Convert 'tip' to a numeric dtype
+tips['tip'] = pd.to_numeric(tips['tip'], errors='coerce')
+
+# Print the info of tips
+print(tips.info())
+
+# Import the regular expression module
+import re
+
+# Compile the pattern: prog
+prog = re.compile('\d{3}-\d{3}-\d{4}')
+
+# See if the pattern matches
+result = prog.match('123-456-7890')
+print(bool(result))
+
+# See if the pattern matches
+result2 = prog.match('1123-456-7890')
+print(bool(result2))
+
+# Import the regular expression module
+import re
+
+# Find the numeric values: matches
+matches = re.findall('\d+', 'the recipe calls for 10 strawberries and 1 banana')
+
+# Print the matches
+print(matches)
+
+# Write the first pattern
+pattern1 = bool(re.match(pattern='\d{3}-\d{3}-\d{4}', string='123-456-7890'))
+print(pattern1)
+
+# Write the second pattern
+pattern2 = bool(re.match(pattern='\$\d*\.\d{2}', string='$123.45'))
+print(pattern2)
+
+# Write the third pattern
+pattern3 = bool(re.match(pattern='[A-z]\w*', string='Australia'))
+print(pattern3)
+
+def recode_sex(sex_value):
+
+    # Return 1 if sex_value is 'Male'
+    if sex_value == 'Male':
+        return 1
+    
+    # Return 0 if sex_value is 'Female'    
+    elif sex_value == 'Female':
+        return 0
+    
+    # Return np.nan    
+    else:
+        return np.nan
+
+# Apply the function to the sex column
+tips['sex_recode'] = tips.sex.apply(recode_sex)
+
+# Print the first five rows of tips
+print(tips.head())
+
+# Write the lambda function using replace
+tips['total_dollar_replace'] = tips.total_dollar.apply(lambda x: x.replace('$', ''))
+
+# Write the lambda function using regular expressions
+tips['total_dollar_re'] = tips.total_dollar.apply(lambda  x: re.findall('\d+\.\d+', x)[0])
+
+# Print the head of tips
+print(tips.head())
+
+# Create the new DataFrame: tracks
+tracks = billboard[['year', 'artist', 'track', 'time']]
+
+# Print info of tracks
+print(tracks.info())
+
+# Drop the duplicates: tracks_no_duplicates
+tracks_no_duplicates = tracks.drop_duplicates()
+
+# Print info of tracks
+print(tracks_no_duplicates.info())
+
+# Calculate the mean of the Ozone column: oz_mean
+oz_mean = airquality.Ozone.mean()
+
+# Replace all the missing values in the Ozone column with the mean
+airquality['Ozone'] = airquality['Ozone'].fillna(oz_mean)
+
+# Print the info of airquality
+print(airquality.info())
+
+# Assert that there are no missing values
+assert ebola.notnull().all().all()
+
+# Assert that all values are >= 0
+assert (ebola>=0).all().all()
+
+#putting all together
+
+#Check the data quality issues
+import pandas as pd
+df = pd.read_csv('input.csv')
+df.head()
+df.info()
+df.columns
+df.describe()
+df.column.value_counts()
+df.column.hist()
+df.dtypes
+df.apply(cleaning_function, axis=0) #axis=1 for columns
+assert (df.column_data > 0).all()
+pd.merge(df1, df2)
+pd.concat([df1, df2])
+
+def check_null_or_valid(row_data):
+    """Function that takes a row of data,
+    drops all missing values,
+    and checks if all remaining values are greater than or equal to 0
+    """
+    no_na = row_data.dropna()[1:-1]
+    numeric = pd.to_numeric(no_na)
+    ge0 = numeric >= 0
+    return ge0
+
+# Check whether the first column is 'Life expectancy'
+assert g1800s.columns[0] == 'Life expectancy'
+
+# Check whether the values in the row are valid
+assert g1800s.iloc[:, 1:].apply(check_null_or_valid, axis=1).all().all()
+
+# Check that there is only one instance of each country
+assert g1800s['Life expectancy'].value_counts()[0] == 1
+
+# Concatenate the DataFrames row-wise
+gapminder = pd.concat([g1800s, g1900s, g2000s])
+
+# Print the shape of gapminder
+print(gapminder.shape)
+
+# Print the head of gapminder
+print(gapminder.head())
+
+# Melt gapminder: gapminder_melt
+gapminder_melt = pd.melt(gapminder, id_vars='Life expectancy')
+
+# Rename the columns
+gapminder_melt.columns = ['country', 'year', 'life_expectancy']
+
+# Print the head of gapminder_melt
+print(gapminder_melt.head())
+
+# Convert the year column to numeric
+gapminder.year = pd.to_numeric(gapminder.year)
+
+# Test if country is of type object
+assert gapminder.country.dtypes == np.object
+
+# Test if year is of type int64
+assert gapminder.year.dtypes == np.int64
+
+# Test if life_expectancy is of type float64
+assert gapminder.life_expectancy.dtypes == np.float64
+
+# Create the series of countries: countries
+countries = gapminder.country
+
+# Drop all the duplicates from countries
+countries = countries.drop_duplicates()
+
+# Write the regular expression: pattern
+pattern = '^[A-Za-z\s\.]*$'
+
+# Create the Boolean vector: mask
+mask = countries.str.contains(pattern)
+
+# Invert the mask: mask_inverse
+mask_inverse = ~mask
+
+# Subset countries using mask_inverse: invalid_countries
+invalid_countries = countries.loc[mask_inverse]
+
+# Print invalid_countries
+print(invalid_countries)
+
+# Assert that country does not contain any missing values
+assert pd.notnull(gapminder.country).all()
+
+# Assert that year does not contain any missing values
+assert pd.notnull(gapminder.year).all()
+
+# Drop the missing values
+gapminder = gapminder.dropna()
+
+# Print the shape of gapminder
+print(gapminder.shape)
+
+# Add first subplot
+plt.subplot(2, 1, 1) 
+
+# Create a histogram of life_expectancy
+gapminder.life_expectancy.plot(kind='hist')
+
+# Group gapminder: gapminder_agg
+gapminder_agg = gapminder.groupby('year')['life_expectancy'].mean()
+
+# Print the head of gapminder_agg
+print(gapminder_agg.head())
+
+# Print the tail of gapminder_agg
+print(gapminder_agg.tail())
+
+# Add second subplot
+plt.subplot(2, 1, 2)
+
+# Create a line plot of life expectancy per year
+gapminder_agg.plot()
+
+# Add title and specify axis labels
+plt.title('Life expectancy over the years')
+plt.ylabel('Life expectancy')
+plt.xlabel('Year')
+
+# Display the plots
+plt.tight_layout()
+plt.show()
+
+# Save both DataFrames to csv files
+gapminder.to_csv('gapminder.csv')
+gapminder_agg.to_csv('gapminder_agg.csv')
+
+
+
+
+
