37150389a1cfb103c3057a7b2715868ef117d83a
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-09-25 17:26:42 +0530

    Create stastical_thinking-1.py

---
+++ stastical_thinking-1.py
@@ -0,0 +1,45 @@
+# Import plotting modules
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set default Seaborn style
+sns.set()
+
+# Plot histogram of versicolor petal lengths
+plt.hist(versicolor_petal_length)
+
+# Show histogram
+plt.show()
+
+# Plot histogram of versicolor petal lengths
+_ = plt.hist(versicolor_petal_length)
+
+# Label axes
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('count')
+
+# Show histogram
+plt.show()
+
+# Import numpy
+import numpy as np
+
+# Compute number of data points: n_data
+n_data = len(versicolor_petal_length)
+
+# Number of bins is the square root of number of data points: n_bins
+n_bins = np.sqrt(n_data)
+
+# Convert number of bins to integer: n_bins
+n_bins = int(n_bins)
+
+# Plot the histogram
+plt.hist(versicolor_petal_length, bins=n_bins)
+
+# Label axes
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('count')
+
+# Show histogram
+plt.show()
+
